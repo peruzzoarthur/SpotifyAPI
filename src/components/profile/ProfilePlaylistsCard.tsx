6d540007fc3a16418529ffeff9b6673f45c14e3b
sp3ruzzo <peruzzoarthur@gmail.com>
2023-10-09 23:04:18 -0300

    migrating to tailwind css2

--- src/components/profile/ProfilePlaylistsCard.tsx
+++ src/components/profile/ProfilePlaylistsCard.tsx
@@ -1,25 +1,21 @@
 import { Image } from "@spotify/web-api-ts-sdk";
 
-interface PlaylistsCardProps {
+interface PlaylistInfoCardProps {
   image: Image[];
   name: string;
   totalTracks: number | undefined;
 }
 
-function ProfilePlaylistsCard({
-  image,
-  name,
-  totalTracks,
-}: PlaylistsCardProps) {
+function PlaylistInfoCard({ image, name, totalTracks }: PlaylistInfoCardProps) {
   return (
     <>
-      <div className="flex flex-col items-center text-slate-100 bg-white bg-opacity-20 drop-shadow-lg rounded-lg p-4 w-64 h-72 mt-4 mb-4 ml-4 mr-4">
-        <img className="max-h-48 max-w-48" src={image[0].url} alt={name} />
-        <p className="mt-2">{name}</p>
-        <p>Total Tracks: {totalTracks}</p>
+      <div className="flex flex-col items-center text-slate-100 bg-white bg-opacity-20 drop-shadow-lg rounded-lg p-4 mb-4 ml-4 mr-4">
+        <img className="max-h-40 max-w-full" src={image[0].url} alt={name} />
+        <p className="mt-2 text-xs">{name}</p>
+        <p className="text-xs">Total Tracks: {totalTracks}</p>
       </div>
     </>
   );
 }
 
-export default ProfilePlaylistsCard;
+export default PlaylistInfoCard;
--- src/components/profile/ProfilePlaylistsSection.tsx
+++ src/components/profile/ProfilePlaylistsSection.tsx
@@ -10,16 +10,15 @@ function ProfilePlaylistsSection({ playlists }: ProfilePlaylistSectionProps) {
   return (
     <>
       <section className="bg-white bg-opacity-10 w-full h-auto">
-        <h1 className="text-white text-8xl ml-8 pt-10">Playlists</h1>
-        <div className="grid grid-rows-2 grid-cols-6 ml-5 mt-5">
+        <h1 className="text-white text-8xl">Playlists</h1>
+        <div className="grid grid-rows-2 grid-cols-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 ml-5 mt-5">
           {playlists.map((playlist, index) => (
-            <Link to={`/playlists/${playlist.id}`} key={index}>
-              <ProfilePlaylistsCard
-                image={playlist.images}
-                name={playlist.name}
-                totalTracks={playlist.tracks?.total}
-              />
-            </Link>
+            <ProfilePlaylistsCard
+              key={index}
+              image={playlist.images}
+              name={playlist.name}
+              totalTracks={playlist.tracks?.total}
+            />
           ))}
         </div>
       </section>
