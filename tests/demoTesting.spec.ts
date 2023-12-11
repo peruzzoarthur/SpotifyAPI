f0d9f1826f879ae933688bd3a6df3b70034553b8
sp3ruzzo <peruzzoarthur@gmail.com>
2023-12-11 18:07:07 -0300

    added playwright and started mocking the request routes with msw, using some spotifys responses... e2e testing

--- tests/demoTesting.spec.ts
+++
@@ -1,8 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('test', async ({ page }) => {
-  await page.locator('body').click();
-  await page.locator('body').press('F12');
-  await page.goto('about:blank');
-  await page.goto('chrome-error://chromewebdata/');
-});
